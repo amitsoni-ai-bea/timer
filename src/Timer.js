f9989ab040b127754a256a3e63f0e480cd175a77
amitkumar.niranjan <95871470+amitkumar-niranjan@users.noreply.github.com>
2025-07-08 08:13:16 +0530

    RESUME BUTTON AND NOW STATE IS REMOVED AS IT IS NOT REQUIRED

--- src/Timer.js
+++ src/Timer.js
@@ -3,27 +3,23 @@ import React, { useState, useRef } from "react";
 const Timer = () =>{
 
     const [timer, setTimer] = useState(0);
-    const [now, setNow]= useState(0);
-    let recorTime = useRef(null);;
+    let recordTime = useRef(null);;
 
     const timerStartHandler = () =>{
-       recorTime.current = setInterval(() =>{
+       recordTime.current = setInterval(() =>{
             setTimer(prevTimer => prevTimer + 1);
-            setNow(prevNow => prevNow + 1);
             console.log(`Timer: ${timer}`);
-            console.log(`Now: ${now}`);
         }, 1000);
     }
 
     const timerPauseHandler = () =>{
-        clearInterval(recorTime.current);
+        clearInterval(recordTime.current);
     }
     return (
         <>
         <p>Timer: {timer}</p>
         <button onClick={timerStartHandler}>Start</button>
         <button onClick={timerPauseHandler}>Pause</button>   
-        <button onClick={timerStartHandler}>Resume</button>
         </>
     );
 }
